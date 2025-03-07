4fb612e4fbe5a2a04d04ec0ef421c022e725182a
shivareddysama <samashivareddy9002@gmail.com>
2025-03-06 21:32:08 -0500

    Initial commit with Terraform and Husky setup

---
+++ infrastructure/main.tf
@@ -0,0 +1,19 @@
+# Azure Provider Configuration
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East US"
+}
+
+# Create a storage account
+resource "azurerm_storage_account" "example" {
+  name                     = "examplestorageacc123"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
